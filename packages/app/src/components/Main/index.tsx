029150cae914f5a93e1715e48204da4ba9c1c75a
andrewyan-taroko <andrew.yan@tarokosoftware.com>
2019-05-23 10:53:37 +0800

    husky running test test

--- packages/app/src/components/Main/index.tsx
+++ packages/app/src/components/Main/index.tsx
@@ -1,5 +1,5 @@
 import React, { ReactElement } from 'react';
-import { Button, Images, Layout } from '@my-pkg/components-webpack';
+import { Button, Images, Layout } from '@mypkg/components';
 import styled from 'styled-components';
 
 const Wrapper = styled.div`
@@ -11,7 +11,7 @@ const App = (): ReactElement => (
     <Button>aaa</Button>
     <Images />
     <Layout title="I am layout">
-      <h1>I am children of layout</h1>
+      <h1>I am children of layout meow</h1>
     </Layout>
   </Wrapper>
 );
